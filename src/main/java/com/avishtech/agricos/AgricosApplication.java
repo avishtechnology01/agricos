10d1db7564650f99ddc75835b9c6e76a548364e5
akshay patil <akshay1999p@gmail.com>
2022-03-18 15:56:59 +0530

    initial commit

---
+++ src/main/java/com/avishtech/agricos/AgricosApplication.java
@@ -0,0 +1,13 @@
+package com.avishtech.agricos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avishtech/agricos/AgricosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishtech.agricos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
